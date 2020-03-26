ec3b8f17f8e436477a60f06d0e075f0b8279ed41
Miraj <mirajkhan14@gmail.com>
2020-03-26 20:28:12 +0600

    update

---
+++ firebase config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCKzBFuDVrfmMvcyIYzrRCgZdBGlbZ7UuU",
+    authDomain: "ema-john-simple-8f153.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-8f153.firebaseio.com",
+    projectId: "ema-john-simple-8f153",
+    storageBucket: "ema-john-simple-8f153.appspot.com",
+    messagingSenderId: "663386962294",
+    appId: "1:663386962294:web:38680bd63e1ecfa205e257"
+  };
+
+  export default firebaseConfig;
